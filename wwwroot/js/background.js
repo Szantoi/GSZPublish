a2631d02e4344de47bbee1902c39de45e60d6d18
Szantoi <szantoi.g@gmail.com>
2024-04-12 19:47:06 +0200

    cyber punk sty 00.0.01

---
+++ wwwroot/js/background.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var bg = document.getElementById("bg");
+    var bgh = document.getElementById("bgh");
+    var bgm = document.getElementById("bgm");
+    var animationDuration = 5000;
+
+    bg.style.backgroundImage = "url('/Image/Backgrund.png')";
+    bgh.style.backgroundImage = "url('/Image/Backgrund_header.gif')";
+    bgm.style.backgroundImage = "url('/Image/Backgrund_main.png')";
+
+    setTimeout(function () {
+        bgh.style.backgroundImage = "url('/Image/Backgrund_header.png')";
+    }, animationDuration);
+});
