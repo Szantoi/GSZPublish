0d0f354a4986447e4868ce7d6916d828a16ed694
Szantoi <szantoi.g@gmail.com>
2024-04-13 21:44:10 +0200

    Style Font 00.0.01

--- wwwroot/js/background.js
+++ wwwroot/js/background.js
@@ -1,14 +1,13 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
     var bg = document.getElementById("bg");
     var bgh = document.getElementById("bgh");
-    var bgm = document.getElementById("bgm");
     var animationDuration = 5000;
 
-    bg.style.backgroundImage = "url('/Image/Backgrund.png')";
     bgh.style.backgroundImage = "url('/Image/Backgrund_header.gif')";
-    bgm.style.backgroundImage = "url('/Image/Backgrund_main.png')";
+
+    bg.style.backgroundImage = "url('/Image/Backgrund.png')";
 
     setTimeout(function () {
-        bgh.style.backgroundImage = "url('/Image/Backgrund_header.png')";
+        bgh.style.backgroundImage = "url('/Image/Backgrund_header_2.png')";
     }, animationDuration);
 });
